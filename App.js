2de1b6b2c85fbedb2242c4b97e1b3c7aafdd6e75
dylangriess <dylangriess@gmail.com>
2022-12-15 18:35:02 -0700

    preserved landing, modularized flexbox for education

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import Landing from "./pages/Landing/Landing";
+import Flexbox from "./pages/Flexbox/Flexbox";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Landing />
+      <Flexbox />
       <StatusBar style="auto" />
     </View>
   );
