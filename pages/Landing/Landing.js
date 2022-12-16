a5589a98f7eb54e1bc20c7dc628e99ac59c900bd
dylangriess <dylangriess@gmail.com>
2022-12-15 18:19:44 -0700

    imported landing successfully using View

--- pages/Landing/Landing.js
+++ pages/Landing/Landing.js
@@ -1,7 +1,12 @@
 import React from "react";
+import { View, Text } from "react-native";
 
 function Landing() {
-  return <div>Landing</div>;
+  return (
+    <View>
+      <Text>Hello</Text>
+    </View>
+  );
 }
 
 export default Landing;
